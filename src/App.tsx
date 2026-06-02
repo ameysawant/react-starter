00f18d9035963d6bc0b492d2cd0074e87bd390be
Amey <mailforamey@gmail.com>
2026-06-02 17:49:03 +0530

    updated global.css and corrected the oorder of tailwind imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { RouterProvider } from "react-router";
 import { router } from "@/router/routes/Router";
+import { Button } from "./shared/components/shadcn-ui/button";
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Button>Click me</Button>
+    </>
+  );
 };
 
 export default App;
