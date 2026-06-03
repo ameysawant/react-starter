568937ddb07becb047a3c01810e1df6ea4ac54f8
Amey <mailforamey@gmail.com>
2026-06-03 13:23:12 +0530

    added shadcn ui components

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { TooltipProvider } from "@/shared/components/shadcn-ui/tooltip";
 import "@/shared/styles/global.css";
 import App from "@/App.tsx";
 
@@ -9,7 +10,9 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <TooltipProvider>
+        <App />
+      </TooltipProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
